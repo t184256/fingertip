2254ce028c4170ba174ee99153f4d0ace0105545
Alexander Sosedkin <asosedkin@redhat.com>
2019-11-28 02:07:18 +0100

    alpine: add Alpine distro

---
+++ fingertip/plugins/os/alpine.py
@@ -0,0 +1,81 @@
+# Licensed under GNU General Public License v3 or later, see COPYING.
+# Copyright (c) 2019 Red Hat, Inc., see CONTRIBUTORS.
+
+import types
+
+import fingertip
+from fingertip.util import path
+
+
+MIRROR = 'http://dl-cdn.alpinelinux.org/alpine'
+ISO = MIRROR + '/v3.10/releases/x86_64/alpine-virt-3.10.3-x86_64.iso'
+REPO = MIRROR + '/v3.10/main/'
+
+
+def main(m=None):
+    m = m or fingertip.build('backend.qemu', ram_size='128M')
+
+    with open(path.fingertip('ssh_key', 'fingertip.pub')) as f:
+        ssh_pubkey = f.read().strip()
+
+    path_to_iso = m.http_cache.fetch(ISO)
+
+    with m:
+        m.qemu.run(load=None, extra_args=['-cdrom', path_to_iso])
+        m.console.expect_exact('localhost login: ')
+        m.console.sendline('root')
+        m.console.expect_exact('localhost:~# ')
+
+        m.hostname = 'alpine'
+        m.prompt = f'{m.hostname}:~# '
+
+        m.console.sendline('setup-alpine -q')
+        m.console.expect_exact('Select keyboard layout [none]:')
+        m.console.sendline('us')
+        m.console.expect_exact('Select variant []:')
+        m.console.sendline('us')
+        m.console.expect_exact(m.prompt)
+
+        m.console.sendline(f'setup-proxy {m.http_cache.internal_url}')
+        m.console.expect_exact(m.prompt)
+        m.console.sendline('. /etc/profile.d/proxy.sh')
+        m.console.expect_exact(m.prompt)
+
+        m.console.sendline(f'setup-apkrepos {REPO}')
+        m.console.expect_exact(m.prompt)
+
+        m.console.sendline('setup-sshd -c dropbear')
+        m.console.expect_exact(m.prompt)
+
+        m.console.sendline('setup-disk -m sys /dev/vda')
+        m.console.expect_exact('Erase the above disk(s) and continue? [y/N]:')
+        m.console.sendline('y')
+        m.console.expect_exact(m.prompt)
+        m.console.sendline('poweroff')
+
+        m.qemu.wait()
+        m.qemu.compress_image()
+
+        m.qemu.run(load=None)
+        m.console.expect_exact(f'{m.hostname} login: ')
+        m.console.sendline('root')
+        m.console.expect_exact(m.prompt)
+
+        m.console.sendline(f'install -m 700 -d .ssh')
+        m.console.expect_exact(m.prompt)
+        m.console.sendline(f'echo "{ssh_pubkey}" >> .ssh/authorized_keys')
+        m.console.expect_exact(m.prompt)
+
+        m.hook(unseal=unseal)
+
+        return m
+
+
+def unseal(m):
+    with m:
+        m.console.sendline(f'unset http_proxy https_proxy ftp_proxy')
+        m.console.expect_exact(m.prompt)
+        m.ssh('rm /etc/profile.d/proxy.sh')
+        m.ssh('/sbin/setup-proxy none || true')
+        m.ssh('/etc/init.d/networking restart')
+        return m
