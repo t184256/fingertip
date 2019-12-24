6a747decf7662ede0783c0f9aea696338691ad5b
Alexander Sosedkin <asosedkin@redhat.com>
2019-12-24 20:15:14 +0100

    test: test more, skip fedora + greetings

--- test.sh
+++ test.sh
@@ -2,11 +2,22 @@
 set -uex
 
 FINGERTIP='/usr/bin/python3 .'
-BASES=('backend.podman-criu centos' 'os.fedora')
-TESTS=('greeting' 'prompts' 'subshell' 'wait_for_it')
+BASES=('backend.podman-criu centos' 'os.fedora' 'os.alpine')
+TESTS=(
+	self_test.greeting
+	self_test.prompts
+	self_test.subshell
+	self_test.wait_for_it
+	'ssh true'
+)
 
 for BASE in "${BASES[@]}"; do
 	for TEST in "${TESTS[@]}"; do
-		$FINGERTIP $BASE + self_test.$TEST
+		[[ $TEST = self_test.greetings && $BASE = os.fedora ]] \
+			&& continue  # takes too long
+		[[ $TEST = 'ssh true' && $BASE =~ backend.podman-criu* ]] \
+			&& continue  # doesn't have ssh
+
+		$FINGERTIP $BASE + $TEST
 	done
 done
