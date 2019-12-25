218b23729706f2d303fa7454a28f99d48f0ca3bc
Alexander Sosedkin <asosedkin@redhat.com>
2019-12-25 17:22:14 +0100

    unseal: fix it

--- fingertip/plugins/unseal.py
+++ fingertip/plugins/unseal.py
@@ -1,5 +1,10 @@
+# Licensed under GNU General Public License v3 or later, see COPYING.
+# Copyright (c) 2019 Red Hat, Inc., see CONTRIBUTORS.
+
+
 def main(m):
     if m.sealed:
+        m.sealed = False
         with m:
-            m.unseal()
+            m.hooks.unseal(m)
             return m
