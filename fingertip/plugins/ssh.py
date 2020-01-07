4a940fee787a30b5e83c8e8395f514aad2aade40
Alexander Sosedkin <asosedkin@redhat.com>
2020-01-07 11:32:02 +0100

    ssh: add non-transient .exec()

--- fingertip/plugins/ssh.py
+++ fingertip/plugins/ssh.py
@@ -5,23 +5,23 @@ from fingertip.util import log
 
 
 @fingertip.transient
-def main(m, cmd=None):
-    assert not m._up_counter
-
-    m = m.apply('unseal')
-
-    with m.transient() as m:
+def main(m):
+    with m.apply('unseal').transient() as m:
         log.info(f'waiting for the SSH server to be up...')
-        if cmd is None:
-            m.ssh('true')
-            log.info(f'starting interactive SSH session, {m.ssh.port}')
-            subprocess.run(['ssh',
-                            '-o', 'StrictHostKeyChecking=no',
-                            '-o', 'GSSAPIAuthentication=no',
-                            '-o', 'GSSAPIKeyExchange=no',
-                            '-i', m.ssh.key_file,
-                            '-p', str(m.ssh.port),
-                            '-t',
-                            'root@127.0.0.1'])
-        else:
-            m.ssh(cmd)
+        m.ssh('true')
+        log.info(f'starting interactive SSH session, {m.ssh.port}')
+        subprocess.run(['ssh',
+                        '-o', 'StrictHostKeyChecking=no',
+                        '-o', 'GSSAPIAuthentication=no',
+                        '-o', 'GSSAPIKeyExchange=no',
+                        '-i', m.ssh.key_file,
+                        '-p', str(m.ssh.port),
+                        '-t',
+                        'root@127.0.0.1'])
+
+
+def exec(m, cmd):
+    with m.apply('unseal') as m:
+        log.info(f'waiting for the SSH server to be up...')
+        m.ssh(cmd)
+    return m
