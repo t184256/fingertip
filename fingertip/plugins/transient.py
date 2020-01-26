4d2273acd5baca36045c2f38dd3875be96acafe5
Alexander Sosedkin <asosedkin@redhat.com>
2020-01-27 00:31:16 +0100

    transient: add a generic transient wrapper

---
+++ fingertip/plugins/transient.py
@@ -0,0 +1,10 @@
+# Licensed under GNU General Public License v3 or later, see COPYING.
+# Copyright (c) 2019 Red Hat, Inc., see CONTRIBUTORS.
+
+import fingertip
+
+
+@fingertip.transient
+def main(m, next_plugin, *args, **kwargs):
+    with m.transient() as m:
+        m.apply(next_plugin, *args, **kwargs)
