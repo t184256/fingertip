7b770a8cf94c0712a25d409a01805c329ab9c329
Alexander Sosedkin <asosedkin@redhat.com>
2020-04-09 12:27:44 +0200

    script: implement 'natural' transiency rules

--- fingertip/plugins/script.py
+++ fingertip/plugins/script.py
@@ -1,14 +1,31 @@
 # Licensed under GNU General Public License v3 or later, see COPYING.
 # Copyright (c) 2019 Red Hat, Inc., see CONTRIBUTORS.
 
+import fingertip
 
-def run(m, scriptpath, transient=False, no_unseal=False):
+
+# fingertip ... + script.run script  -  no cache, never persist, always rerun
+# fingertip ... + script.run script + ssh  -  cache just for ssh, never reuse
+# fingertip ... + script.run script --cache=1h  -  try to cache for 1h at most
+# fingertip ... + script.run script --cache=1h + ssh  -  try reusing the cache
+
+
+def _should_run_be_transient(scriptpath, cache=0, no_unseal=False):
+    return False if cache else 'last'
+
+
+@fingertip.transient(when=_should_run_be_transient)
+def run(m, scriptpath, cache=0, no_unseal=False):
     m.log.debug(f'Plugin: script, scriptpath: {scriptpath}, '
-                f'transient: {transient}, no_unseal: {no_unseal}')
+                f'cache: {cache}, no_unseal: {no_unseal}')
     m = m if no_unseal else m.apply('unseal')
     scriptname = 'uploaded_script'
-    m.expiration.depend_on_a_file(scriptpath)
+
     with m:
+        if cache is not True:
+            m.expiration.cap(cache)
+        m.expiration.depend_on_a_file(scriptpath)
+
         m.log.debug(f'uploading {scriptpath} into {scriptname}')
         m.ssh.upload(scriptpath, dst=scriptname)
         m.log.debug(f'uploaded {scriptpath} into {scriptname}')
@@ -16,5 +33,4 @@ def run(m, scriptpath, transient=False, no_unseal=False):
         m.log.plain()
         m(f'./{scriptname}')
         m.log.nicer()
-    if not transient:
-        return m
+    return m
