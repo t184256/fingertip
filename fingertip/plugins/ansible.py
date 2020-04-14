9df8b8437905ab0ec99c68b4aab08fd2e537188a
Alexander Sosedkin <asosedkin@redhat.com>
2020-04-14 11:31:23 +0200

    ansible.playbook: update to use .expiration

--- fingertip/plugins/ansible.py
+++ fingertip/plugins/ansible.py
@@ -66,5 +66,6 @@ def main(m, module, *args, **kwargs):
 def playbook(m, playbook_path):
     with m.apply(prepare) as m:
         _ansible(m, playbook_path, cmd=('ansible-playbook',))
-        m.depend_on_a_file(playbook_path)  # not comprehensive; best-effort
+        # not comprehensive; best-effort
+        m.expiration.depend_on_a_file(playbook_path)
     return m
