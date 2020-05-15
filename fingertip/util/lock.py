76c7ae9ea9ecdccb75a4bce141879b985dfc034e
Alexander Sosedkin <asosedkin@redhat.com>
2020-05-15 17:05:01 +0200

    util.lock: get rid of Maybe, inter-thread locking

--- fingertip/util/lock.py
+++ fingertip/util/lock.py
@@ -2,18 +2,37 @@
 Helper functions for fingertep: inter-process locking.
 """
 
+import collections
+import threading
+
 import fasteners
 
 
-class MaybeLock:
-    def __init__(self, path, lock=True):
-        self._lock = lock and fasteners.process_lock.InterProcessLock(path)
+threadlocks = collections.defaultdict(threading.Lock)
+threadlocks_lock = threading.Lock()
+
+
+class Lock:
+    def __init__(self, path):
+        # InterProcessLock does not work across threads
+        self._plock = fasteners.process_lock.InterProcessLock(path)
+        with threadlocks_lock:
+            self._tlock = threadlocks[path]
 
     def __enter__(self):
-        if self._lock:
-            self._lock.acquire()
+        self._plock.acquire()
+        self._tlock.acquire()
         return self
 
     def __exit__(self, *_):
-        if self._lock:
-            self._lock.release()
+        self._tlock.release()
+        self._plock.release()
+
+
+# Python 3.7 has `contextlib.nullcontext`
+class NoLock:
+    def __enter__(self):
+        pass
+
+    def __exit__(self, *_):
+        pass
