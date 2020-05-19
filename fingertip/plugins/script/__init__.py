32f4a6e5749cf73ff745df4cb2b1ecbf73b78446
Alexander Sosedkin <asosedkin@redhat.com>
2020-05-19 14:34:57 +0200

    script: move run and test into a separate file

---
+++ fingertip/plugins/script/__init__.py
@@ -0,0 +1,4 @@
+# Licensed under GNU General Public License v3 or later, see COPYING.
+# Copyright (c) 2019 Red Hat, Inc., see CONTRIBUTORS.
+
+from fingertip.plugins.script._run_and_test import run, test
