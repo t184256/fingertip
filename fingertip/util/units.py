975567a4ba562db34c2dc0a66246c93867253cc9
Alexander Sosedkin <asosedkin@redhat.com>
2020-09-04 13:30:04 +0200

    util.unit: support unsuffixed time interval parsing

--- fingertip/util/units.py
+++ fingertip/util/units.py
@@ -1,8 +1,6 @@
 # Licensed under GNU General Public License v3 or later, see COPYING.
 # Copyright (c) 2020 Red Hat, Inc., see CONTRIBUTORS.
 
-import numbers
-
 TIME = {'s': 1, 'm': 60, 'h': 3600, 'd': 24 * 3600, 'w': 24 * 3600 * 7}
 BINARY = {'K': 2**10, 'M': 2**20, 'G': 2**30, 'T': 2**40, 'P': 2**50}
 
@@ -10,9 +8,10 @@ BINARY = {'K': 2**10, 'M': 2**20, 'G': 2**30, 'T': 2**40, 'P': 2**50}
 def parse_time_interval(interval):
     if isinstance(interval, str) and interval[-1] in TIME:
         return float(interval[:-1]) * TIME[interval[-1]]
-    if isinstance(interval, numbers.Real) and not isinstance(interval, bool):
+    try:
         return float(interval)
-    raise ValueError(f'Cannot parse time interval {interval}')
+    except ValueError:
+        raise ValueError(f'Cannot parse time interval {interval}')
 
 
 def parse_binary(value):
