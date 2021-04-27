977837db4c706a80c8cbdd5aaa72d470f41108d5
Alexander Sosedkin <asosedkin@redhat.com>
2021-04-27 15:33:46 +0200

    ssh: hacky but quick way to attach to existing VMs

--- fingertip/plugins/ssh.py
+++ fingertip/plugins/ssh.py
@@ -1,10 +1,14 @@
+import re
 import subprocess
 
 import fingertip
+from fingertip.util import path
 
 
 @fingertip.transient(when='last')
-def main(m, unseal=True):
+def main(m=None, unseal=True):
+    if m is None:
+        return existing()
     m = m.apply('unseal') if unseal else m
     with m:
         m.expiration.cap(0)  # non-deterministic user input, never reuse
@@ -12,16 +16,37 @@ def main(m, unseal=True):
         m.ssh.exec('true')
         # terminate the ssh session not to leave any traces in vm
         m.ssh.invalidate()
-
         m.log.info(f'starting interactive SSH session, {m.ssh.port}')
         m.log.plain()
+        _connect(m.ssh.port, m.ssh.key_file)
+    return m
+
+
+def _connect(port, key_file):
         subprocess.run(['ssh',
                         '-o', 'StrictHostKeyChecking=no',
                         '-o', 'UserKnownHostsFile=/dev/null',
                         '-o', 'GSSAPIAuthentication=no',
                         '-o', 'GSSAPIKeyExchange=no',
-                        '-i', m.ssh.key_file,
-                        '-p', str(m.ssh.port),
+                        '-i', key_file,
+                        '-p', str(port),
                         '-t',
                         'root@127.0.0.1'], check=True)
-    return m
+
+
+def existing():
+    # HACK HACK HACK
+    key_file = path.fingertip('ssh_key', 'fingertip')
+    process = subprocess.Popen(['ps', '-uf'], stdout=subprocess.PIPE)
+    stdout, _ = process.communicate()
+    ports = [int(p) for p in
+             re.findall(r'hostfwd=tcp:127.0.0.1:(\d+)-:22', stdout.decode())]
+    if len(ports) == 1:
+        return _connect(ports[0], key_file)
+    else:
+        print('several fingertip VMs found, which port?')
+        for i, p in enumerate(ports):
+            print(f'[{i}] {p}')
+        c = int(input('> '))
+        return _connect(ports[c] if c < len(ports) else c, key_file)
+    print('no running fingertip VMs found')
