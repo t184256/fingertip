9a10c176ea8732e648cd8e314fed9e63d0b0c510
Alexander Sosedkin <asosedkin@redhat.com>
2023-04-19 19:40:04 +0200

    os.fedora: f38 is released

--- fingertip/plugins/os/fedora.py
+++ fingertip/plugins/os/fedora.py
@@ -13,7 +13,7 @@ from fingertip.plugins.os.common import red_hat_based
 FEDORA_GEOREDIRECTOR = 'http://download.fedoraproject.org/pub/fedora/linux'
 F35_FIX_URL = ('http://rvykydal.fedorapeople.org/update-images/'
                'updates.f35-2019579-resolvconf.img')
-RELEASED = 37
+RELEASED = 38
 
 def prepare_upgrade(m, releasever=None):
     assert hasattr(m, 'fedora')
