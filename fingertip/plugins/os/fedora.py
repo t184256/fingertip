4b18cf9742fd7e84f1460721d2602b1038d91a1f
Alexander Sosedkin <asosedkin@redhat.com>
2024-04-30 12:28:47 +0200

    os.fedora: f40 is released

--- fingertip/plugins/os/fedora.py
+++ fingertip/plugins/os/fedora.py
@@ -11,7 +11,7 @@ from fingertip.plugins.os.common import red_hat_based
 
 
 FEDORA_GEOREDIRECTOR = 'http://download.fedoraproject.org/pub/fedora/linux'
-RELEASED = 39
+RELEASED = 40
 
 def prepare_upgrade(m, releasever=None):
     assert hasattr(m, 'fedora')
