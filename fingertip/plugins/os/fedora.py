0c39d23d6e41bd930575aae95e62f27ad30db083
Clemens Lang <cllang@redhat.com>
2024-10-31 17:42:07 +0100

    os.fedora: Update to 41

--- fingertip/plugins/os/fedora.py
+++ fingertip/plugins/os/fedora.py
@@ -11,7 +11,7 @@ from fingertip.plugins.os.common import red_hat_based
 
 
 FEDORA_GEOREDIRECTOR = 'http://download.fedoraproject.org/pub/fedora/linux'
-RELEASED = 40
+RELEASED = 41
 
 def prepare_upgrade(m, releasever=None):
     assert hasattr(m, 'fedora')
